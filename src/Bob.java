995fe54a2f13f91f2ac05c91139fe14eea3b6361
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-09-29 14:48:34 -0500

    bob

--- src/Bob.java
+++ src/Bob.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Bob {
 
-    public static void main(String[] args) {
+    public static void main(String[] a2rgs) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ask a question and make sure to end with a question mark");
-        String userInput = sc.next();
+        String userInput = sc.nextLine();
 
         if(userInput.endsWith("?")){
             System.out.println("Sure.");
         }else if(userInput.endsWith("!")){
             System.out.println("Whoa, chill out dude");
-        }else if(userInput.endsWith(" ")){
+        }else if(userInput.isEmpty()){
             System.out.println("fine be that way");
         }else{
             System.out.println("whatever");
