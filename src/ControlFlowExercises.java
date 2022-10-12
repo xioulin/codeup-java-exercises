fe85d32e7ffff0d67a801420926f1f4a1f9087fa
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-12 13:39:02 -0500

    control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,37 +2,48 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+//        //while loop practice
 //        int i = 5;
 //        while (i <= 15){
 //            i++;
-//            System.out.println(i-1);
-//        }]
+//            System.out.print(i-1+" ");
+//        }
+//       // do while loop practice, start from 0 and count to 100 by twos.
 //        i=0;
 //        do{
 //            i+=2;
 //            System.out.println(i);
 //        }while(i <100);
-
+////        //do while loop, start from 100 go down by 5s to -10.
 //        i=100;
 //        do{
 //            i -=5;
 //            System.out.println(i);
 //        }while(i>-9);
-
+        //print out a number getting squared each time starting with two
 //        long j=2;
 //        do{
 //            j = j*j;
 //            System.out.println(j);
-//        }while(j<=1000000);
-
-
-//        for (long y = 2; y < 1000000; y = y * y) {
-//            if (y == 1000000) {
-//                continue;
-//            }
-//            System.out.println(y);
+//        }while(j<=10000);
+//        // refactor the above using for loops, starting with print out from 5 to 15
+//        for(i=5; i<=15; i++){
+//            System.out.print(i+" ");
+//        }
+//        //print out from 0 to 100 by twos
+//        for(i=0;i<=100;i+=2){
+//            System.out.println(i);
+//        }
+//        //print out from 100 down to -10 by 5s
+//        for(i=100;i>-11;i-=5){
+//            System.out.println(i);
+//        }
+//        for(j=2;j<100000;j= j*j){
+//            System.out.println(j);
 //        }
 
+
+//the fizz buzz exercise. every multiple of three prints out Fizz, every mult of 5 prints Buzz, if both, prints FizzBuzz
 //        for (int i = 1; i <= 100; i += 1) {
 //            if (i % 3 == 0 && i % 5 == 0) {
 //                System.out.println("FizzBuzz");
@@ -45,11 +56,70 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter an integer:");
-        int userIntInput = scanner.nextInt();
-        System.out.println(userIntInput*userIntInput);
+        //Table of Powers program
+//        boolean continueProgram;
+//        Scanner scannery = new Scanner(System.in);
+//        do {
+//            System.out.print("What number would you like to go up to?: ");
+//            int userInt = scannery.nextInt();
+//            System.out.println("Here is your table!");
+//            System.out.println("Number | Squared | Cubed");
+//            System.out.println("------   -------   ------");
+//            for (int z = 1; z <= userInt; z++) {
+//                System.out.println(z + "      |   " + z * z + "     |   " + z * z * z);
+//            }
+//            System.out.println("Would you like to continue? y/n");
+//            Scanner scanny = new Scanner(System.in);
+//            String contUser = scanny.nextLine();
+//            if(contUser.equals("y")){
+//                continueProgram = true;
+//            }else{
+//                continueProgram = false;
+//                System.out.println("program terminated, goodbye");
+//            }
+//        }while(continueProgram);
 
-        }
+
+        Scanner scannernavia = new Scanner(System.in);
+        Boolean keepGoing;
+        do {
+            System.out.println("do you wish to continue? enter y if yes");
+            String continuing = scannernavia.nextLine();
+            if(continuing.equals("y")){
+                keepGoing=true;
+            }else{
+                keepGoing=false;
+            }
+            System.out.println("Please enter a number from 0 to 100: ");
+            int userInt = scannernavia.nextInt();
+            for(int z=0;z<userInt;z++){
+                if (userInt < 0 || userInt > 100) {
+                    System.out.println("out of range, try again");
+                } else if (userInt < 100 && userInt >= 88) {
+                    System.out.println("A");
+                } else if (userInt < 88 && userInt >= 80) {
+                    System.out.println("B");
+                } else if (userInt < 80 && userInt >= 67) {
+                    System.out.println("C");
+                } else if (userInt < 67 && userInt >= 60) {
+                    System.out.println("D");
+                } else if (userInt < 60 && userInt >= 0) {
+                    System.out.println("F, you have failed");
+                }
+                System.out.println("do you wish to continue? enter y if yes");
+
+
+
+
+            }
+
+
+
+        }while(keepGoing);
+
 
     }
+
+
+
+}
--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -7,67 +7,56 @@ import java.util.Scanner;
 public class GradesApplication {
 
     public static void main(String[] args) {
-
-        Student joshua = new Student("joshua","44");
+        //create new student classes with student name and initialized grade
+        Student joshua = new Student("joshua", "44");
+        //adding subsequent grades
         joshua.addGrade(94);
         joshua.addGrade(65);
         joshua.addGrade(78);
-        Student elijah = new Student("elijah","55");
+        Student elijah = new Student("elijah", "55");
         elijah.addGrade(77);
         elijah.addGrade(56);
         elijah.addGrade(86);
-        Student samuel = new Student("samuel","69");
+        Student samuel = new Student("samuel", "69");
         samuel.addGrade(12);
         samuel.addGrade(76);
         samuel.addGrade(57);
-        Student jezebel = new Student("jezebel","78");
+        Student jezebel = new Student("jezebel", "78");
         jezebel.addGrade(88);
         jezebel.addGrade(96);
         jezebel.addGrade(92);
-        HashMap<String,Student> students = new HashMap<>();
-        students.put("joshLover",joshua);
-        students.put("elijahProphet666",elijah);
-        students.put("lelekProrok",samuel);
-        students.put("biblicalchick44",jezebel);
+        //create hashmap for student key value pairing. pairing github usernames with student class objects
+        HashMap<String, Student> students = new HashMap<>();
+        students.put("joshJericho", joshua); // e.x. key is github username is "joshLover", value is student object
+        students.put("elijahProphet6", elijah);
+        students.put("lelekProrok", samuel);
+        students.put("biblicalchick44", jezebel);
 
-
-        //returns key from key value
-//        Object gitUserValue = students.get(gitUserName); // returns value from kay value pair
-//        System.out.println(students);
-//        System.out.println(gitUserName);
-//        System.out.println(gitUserValue);
-
-        for (int i = 0; i < students.size(); i++) {
-            System.out.print("  |  "+students.keySet().toArray()[i]);
-        }
-//        ArrayList<String> gitUserNames = new ArrayList<>();
-//        for (int i = 0; i < students.size(); i++) {
-//            gitUserNames.add((String) students.keySet().toArray()[i]);
-//        }
-//        System.out.println(gitUserNames);
         Scanner scannyscanscan = new Scanner(System.in);
-        System.out.print("\nSelect one of the above user names to learn more:");
-        String userInput = scannyscanscan.nextLine();
-            if (students.containsKey(userInput)) {
+        System.out.println("would you like to continue? type yes if so");
+        String continuer = scannyscanscan.nextLine();
+
+            //for loop to print out the key values aka. the git usernames
+            for (int i = 0; i < students.size(); i++) {
+                System.out.print("  |  " + students.keySet().toArray()[i]);
+            }
+            // scanner object created to get user input
+            System.out.print("\nSelect one of the above user names to learn more:");
+            String userInput = scannyscanscan.nextLine();
+            //takes user input and matches against the key objects aka the git usernames
+            if (students.containsKey(userInput)) { // checking if the user input matches a user name
                 students.get(userInput);
-                System.out.println(userInput+"'s name is "+students.get(userInput).getName());
-                System.out.println(userInput+"'s grade point average is "+students.get(userInput).getGradeAverage());
+                System.out.println(userInput + "'s name is " + students.get(userInput).getName()); // prints out name associated with student object
+                System.out.println(userInput + "'s grade point average is " + students.get(userInput).getGradeAverage()); // uses gradeAverage method to return grade
             } else {
                 System.out.println("you did not spell the user name correctly");
             }
-        System.out.println("would you like to keep running? type yes if so");
-            String continuer = scannyscanscan.nextLine();
-            if(continuer = "y"){
-
-            }else{
-                System.out.println("okay, program ended");
-            }
+            System.out.println("would you like to continue? type yes if so");
+            continuer = scannyscanscan.nextLine();
 
 
 
     }
-
-
 
 
 }
